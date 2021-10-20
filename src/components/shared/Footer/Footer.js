eba4cc4351246c465a66bd538206a9df987085d9
Fellipe Leal <85651329+flealbrl@users.noreply.github.com>
2021-10-20 00:55:26 -0300

    Add files via upload

---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="w-100">
+      <div className="container">
+        <p className="text-center"> Desenvolvido por <a class='link-git' href="https://github.com/flealbrl">Fellipe Leal 🤖</a></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
